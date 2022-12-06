6e27c2ce8bb9e9f859cac0f620cf2fa02b21a184
Huike Cheng <huikecheng@outlook.com>
2022-12-06 01:54:05 +0000

    updates on researchers_v2

--- Gov_collaborators.py
+++ Gov_collaborators.py
@@ -64,15 +64,22 @@ def country_to_continent(country_name):
         country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
         return country_continent_name
 # convert
-df_ns['place_of_birth'] = df_ns['country_of_birth'].apply(country_to_continent)
+df_ns['continent_of_birth'] = df_ns['country_of_birth'].apply(country_to_continent)
 # drop country of birth column
 df_ns = df_ns.drop(columns = 'country_of_birth')
 # check numbers in each continent
-a = df_ns.groupby(['place_of_birth']).size().reset_index(name='count')
-#a.loc[a['count']<10]
-#a.loc[a['place_of_birth']=='South America']
+a = df_ns.groupby(['continent_of_birth']).size().reset_index(name='count')
+a.loc[a['count'] < 30]
+# combine Antarctica, Indian Ocean, the Arctic Ocean and South America --> smallest categry contains at least 50 people OR drop the records
+df_ns['continent_of_birth'] = df_ns['continent_of_birth'].replace({'Antarctica': 'Other continents',
+                                                                   'Indian Ocean': 'Other continents',
+                                                                   'the Arctic Ocean': 'Other continents',
+                                                                   'South America': 'Other continents'})
+# check numbers in each continent
+a = df_ns.groupby(['continent_of_birth']).size().reset_index(name='count')
+a
 
-################ postcode --> banding ################
+############################### postcode --> banding ##################################
 ####### keep outbound characters only
 # define function for finding the index of the first digit in a string
 def find_first_digit(s):
@@ -85,23 +92,49 @@ df_ns['postcode'] = df['postcode'].apply(lambda x: x[:find_first_digit(x)])
 # check number in each category
 post_count = df_ns.groupby(['postcode']).size().reset_index(name='count')
 post_count
-# get the postcode with only 1 person
-post_agr = post_count['postcode'].loc[post_count['count'] == 1]
-# group these postcodes together
-df_ns['postcode'] = np.where(df_ns['postcode'].isin(post_agr), '000', df_ns['postcode'])
-# check number in each category
-post_count = df_ns.groupby(['postcode']).size().reset_index(name='count')
-post_count
+# get dictionary for convert to UK country
+postcode_country = pd.read_csv('Data/postcode_country.csv')
+post_to_country = dict(zip(postcode_country['Postcode area'], postcode_country['Country']))
+# convert to country
+df_ns['UK_country'] = df_ns['postcode'].replace(post_to_country)
+# drop postcode column
+df_ns = df_ns.drop(columns = 'postcode')
+# check number of individuals in each category
+a = df_ns.groupby(['UK_country']).size().reset_index(name='count')
+a.loc[a['count'] < 30]
+# combine Channel Islands. Isle of Man, Northern Ireland and Wales
+df_ns['UK_country'] = df_ns['UK_country'].replace({'Channel Islands': 'Other countries',
+                                                   'Isle of Man': 'Other countries',
+                                                   'Northern Ireland': 'Other countries',
+                                                   'Wales': 'Other countries'})
+# check number of individuals in each category
+a = df_ns.groupby(['UK_country']).size().reset_index(name='count')
+a
+
+############### education level ################
+a = df_ns.groupby(['education_level']).size().reset_index(name='count')
+a
+### combine some categories
+df_ns['education_level'] = df_ns['education_level'].replace({'primary': 'Primary & Secondary',
+                                                             'secondary': 'Primary & Secondary',
+                                                             'masters': 'Masters & PhD',
+                                                             'phD': 'Masters & PhD'})
 
 ############### calculate k-anonimity ##################
 df_ns.describe()
 
-# df_ns.groupby(['cc_status', 'postcode']).size().reset_index(name='count')
-a = df_ns.groupby(['cc_status', 'postcode']).size().reset_index(name='count')
-a.loc[a['count']==1]
+# df_ns.groupby(['cc_status', 'UK_country', 'continent of birth', 'education level']).size().reset_index(name='count')
+# 2 * 3 * 6 * 4 = 144
+a = df_ns.groupby(['cc_status', 'UK_country', 'continent_of_birth', 'education_level']).size().reset_index(name='count')
+b = a.loc[a['count']==1]
+a.shape
+b.shape
+# remove the 20 individuals?
+# sample with probability?
 
 # save CSVs
-# sensitive file
-# file for researchers
+# sensitive file: same as the sensitive_info file for researchers
+# file for government collaborators
+
 # df.to_csv(PATH)
 
